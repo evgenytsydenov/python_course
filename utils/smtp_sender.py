0f3c4adb2cef858190c17db88b23f96a8f946a8e
Evgeny Tsydenov <evgenytsydenov@gmail.com>
2022-12-22 19:54:19 +0600

    Update: fixing style, minor refactorings of smtp sender

--- utils/smtp_sender.py
+++ utils/smtp_sender.py
@@ -19,10 +19,11 @@ class SMTPSender:
     ) -> None:
         """Create email sender.
 
-        :param login: mailbox login.
-        :param password: mailbox password.
-        :param server: mail server address.
-        :param server_port: port of mail server.
+        Args:
+            login: Mailbox login.
+            password: Mailbox password.
+            server: Server address.
+            server_port: Port of the mail server.
         """
         self._password = password
         self._login = login
@@ -39,49 +40,84 @@ class SMTPSender:
     ) -> None:
         """Send message.
 
-        :param files: path to files to attach.
-        :param plain_text: text message.
-        :param destination: destination address.
-        :param subject: letter subject.
-        :param html_content: html string.
+        Args:
+            destination: Destination address.
+            subject: Letter subject.
+            plain_text: Text message.
+            html_content: HTML string.
+            files: Path to files to attach.
         """
-        # Create message
-        message = MIMEMultipart()
-        message["From"] = self._login
-        message["To"] = destination
-        message["Subject"] = subject
-        if plain_text:
-            message.attach(MIMEText(plain_text))
-        if html_content:
-            message.attach(MIMEText(html_content, "html"))
-        if files:
-            for file in files:
-                with open(file, "rb") as file_obj:
-                    file_name = os.path.basename(file)
-                    file_attachment = MIMEBase("application", "octet-stream")
-                    file_attachment.set_payload(file_obj.read())
-                    encoders.encode_base64(file_attachment)
-                    file_attachment.add_header(
-                        "Content-Disposition", f'attachment; filename="{file_name}"'
-                    )
-                    message.attach(file_attachment)
-        text = message.as_string()
-
-        # Send
+        message = self._create_message(
+            destination, subject, plain_text, html_content, files
+        )
         server = smtplib.SMTP(self._server, self._server_port)
         tls_context = ssl.create_default_context()
         try:
             server.ehlo()
             server.starttls(context=tls_context)
             server.ehlo()
-            logger.debug(
-                f"Connected to SMTP server: " f"{self._server}:{self._server_port}."
-            )
+            logger.debug("Connected to the SMTP server.")
             server.login(self._login, self._password)
-            logger.debug(
-                f'Authentication with login "{self._login}" was ' f"successful."
-            )
-            server.sendmail(self._login, destination, text)
+            logger.debug("Authentication was successful.")
+            server.sendmail(self._login, destination, message)
             logger.info(f'Message "{subject}" was sent to "{destination}".')
         finally:
             server.quit()
+            logger.debug("Logged out from the SMTP server.")
+
+    def _create_message(
+        self,
+        destination: str,
+        subject: str,
+        plain_text: str | None = None,
+        html_content: str | None = None,
+        files: list[str] | None = None,
+    ) -> str:
+        """Create message.
+
+        Args:
+            destination: Destination address.
+            subject: Letter subject.
+            plain_text: Text message.
+            html_content: Html string.
+            files: Path to files to attach.
+
+        Returns:
+            Message as an encoded string.
+        """
+        message = MIMEMultipart()
+        message["From"] = self._login
+        message["To"] = destination
+        message["Subject"] = subject
+        logger.debug(f'New message "{subject}" to "{destination}" was created.')
+        if plain_text:
+            message.attach(MIMEText(plain_text))
+            logger.debug("Plain text was attached to the message.")
+        if html_content:
+            message.attach(MIMEText(html_content, "html"))
+            logger.debug("HTML content was attached to the message.")
+        files = [] if files is None else files
+        for file in files:
+            message.attach(self._load_attachment(file))
+            logger.debug(f'File "{file}" was attached to the message.')
+        return message.as_string()
+
+    def _load_attachment(self, file_path: str) -> MIMEBase:
+        """Load file content.
+
+        Args:
+            file_path:
+                Path to the file.
+
+        Returns:
+            File content as attachment.
+        """
+        with open(file_path, "rb") as file_obj:
+            file_name = os.path.basename(file_path)
+            file_attachment = MIMEBase("application", "octet-stream")
+            file_attachment.set_payload(file_obj.read())
+            encoders.encode_base64(file_attachment)
+            file_attachment.add_header(
+                "Content-Disposition", f'attachment; filename="{file_name}"'
+            )
+            return file_attachment
