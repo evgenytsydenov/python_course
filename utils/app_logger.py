6bf3b755167f440b22de7e36199905bd4ba34b10
Evgeny Tsydenov <evgenytsydenov@gmail.com>
2022-12-22 19:54:49 +0600

    Update: fixing style, minor refactorings of logger

--- utils/app_logger.py
+++ utils/app_logger.py
@@ -1,7 +1,9 @@
 import logging
 import logging.handlers
 import os
+import time
 from datetime import datetime
+from time import struct_time
 
 import pytz
 
@@ -11,33 +13,42 @@ from definitions import DATE_FORMAT, LOG_FORMAT_DEBUG, LOG_FORMAT_INFO, ROOT_PAT
 class CustomFormatter(logging.Formatter):
     """Override standard Formatter to specify timezone."""
 
-    def converter(self, timestamp: float) -> datetime:
-        """Convert time to UTC zone.
+    def converter(self, timestamp: float | None) -> struct_time:
+        """Get structured UTC time from the timestamp.
 
-        :param timestamp: time in seconds.
-        :return: datetime object in UTC zone.
+        Args:
+            timestamp: Time in seconds.
+
+        Returns:
+            Structured time object.
         """
-        dt = datetime.fromtimestamp(timestamp, tz=pytz.UTC)
-        return dt.astimezone(pytz.timezone("UTC"))
+        return time.gmtime(timestamp)
 
     def formatTime(  # noqa N802
         self, record: logging.LogRecord, datefmt: str | None = None
     ) -> str:
-        """Convert time to specified format.
+        """Convert time to the specified format.
 
-        :param record: log record.
-        :param datefmt: date format.
-        :return: time in string format.
+        Args:
+            record: Log record.
+            datefmt: Date format.
+
+        Returns:
+            Time in string format.
         """
-        dt = self.converter(record.created)
+        struct_dt_utc = self.converter(record.created)
+        dt = datetime(*struct_dt_utc[:6], tzinfo=pytz.UTC)
         return dt.strftime(datefmt) if datefmt else dt.isoformat()
 
 
 def _get_file_handler(path: str) -> logging.FileHandler:
     """Create logger to save logs to a file.
 
-    :param path: path to log file.
-    :return: file handler for logs.
+    Args:
+        path: Path to log file.
+
+    Returns:
+        File handler for logs.
     """
     file_handler = logging.FileHandler(path, encoding="utf-8")
     file_handler.setLevel(logging.DEBUG)
@@ -48,9 +59,10 @@ def _get_file_handler(path: str) -> logging.FileHandler:
 
 
 def _get_stream_handler() -> logging.StreamHandler:
-    """Create logger to print logs to console.
+    """Create logger to print logs to the console.
 
-    :return: stream handler for logs.
+    Returns:
+        Stream handler for logs.
     """
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
@@ -63,15 +75,18 @@ def _get_stream_handler() -> logging.StreamHandler:
 def get_logger(module_name: str, log_file_name: str = "system.log") -> logging.Logger:
     """Create logger.
 
-    :param log_file_name: name of the log file.
-    :param module_name: name of the module where events happen.
-    :return: logger.
+    Args:
+        module_name: Name of the log file.
+        log_file_name: Name of the module where events happen.
+
+    Returns:
+        Logger.
     """
-    logger = logging.getLogger(module_name)
     path_logs = os.path.join(ROOT_PATH, "logs")
-    logger.setLevel(logging.DEBUG)
     if not os.path.exists(path_logs):
         os.mkdir(path_logs)
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.DEBUG)
     logger.addHandler(_get_file_handler(os.path.join(path_logs, log_file_name)))
     logger.addHandler(_get_stream_handler())
     return logger
